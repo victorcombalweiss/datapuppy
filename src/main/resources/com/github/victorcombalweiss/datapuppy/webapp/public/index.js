2b61ca94e50bcac1750a126d8dff0922d2bc247d
Victor Combal-Weiss <victorcombalweiss@users.noreply.github.com>
2019-04-08 15:08:49 +0200

    Fix headers on web interface
    Previously, if user scrolled down on web interface, headers would
    disappear past the top of the page. This was a bad user experience
    and prevented user from seeing the alert banner if it lit up.
    
    This commit fixes it by fixing headers to the top, without them
    scrolling ever.

--- src/main/resources/com/github/victorcombalweiss/datapuppy/webapp/public/index.js
+++ src/main/resources/com/github/victorcombalweiss/datapuppy/webapp/public/index.js
@@ -1,6 +1,7 @@
 const REFRESH_PERIOD = 1000;
 const ROOT_TEMPLATE_ID = "rootTemplate";
 const ROOT_ID = "root";
+const MAIN_BODY_WRAPPER_ID = "mainBodyWrapper";
 
 function refresh() {
     try {
@@ -62,10 +63,24 @@ function sortedErrorCodes(stats) {
 }
 
 function render(data) {
+    let top = 0, left = 0;
+    let mainBodyWrapper = getMainBodyWrapper();
+    if (mainBodyWrapper) {
+        top = mainBodyWrapper.scrollTop;
+        left = mainBodyWrapper.scrollLeft;
+    }
     if (document.getElementById(ROOT_TEMPLATE_ID) && document.getElementById(ROOT_ID)) {
         var template = Handlebars.compile(document.getElementById(ROOT_TEMPLATE_ID).innerHTML);
         document.getElementById(ROOT_ID).innerHTML = template(data);
     }
+    mainBodyWrapper = getMainBodyWrapper();
+    if (mainBodyWrapper) {
+        mainBodyWrapper.scrollTo(left, top);
+    }
+}
+
+function getMainBodyWrapper() {
+    return document.getElementById(MAIN_BODY_WRAPPER_ID);
 }
 
 refresh();
